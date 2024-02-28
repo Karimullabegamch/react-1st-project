53cdc1faa950244fea2a57cf91b01042675e001c
karimulla begam <chkarimullabegam7893@gmail.com>
2024-02-28 09:35:37 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App()
+{ let val=1
+  return (<div >
+   
+    <img src="https://th.bing.com/th/id/OIG2.wlGIYjBA6x09ASnELbfC?w=1024&h=1024&rs=1&pid=ImgDetMain" height="200px" width="250px"  alt='img'/>
+    <h1>butterflys</h1>
+    <p>{val}</p>
+    
+    </div>)
+   
 }
-
 export default App;
---
+++ src/Apps.js
@@ -0,0 +1,14 @@
+import App from  './App'
+function Apps()
+{
+    return (
+        <div id="image">
+        <App />
+        <App />
+        <App />
+        <App />
+        <App />
+          </div>
+    )
+}
+export default Apps
